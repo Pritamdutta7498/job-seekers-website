6dbef9b97c874fb4c06ca4f7627266c9c688b8fe
PritamDutta <webdevpritam7498@gmail.com>
2023-04-12 12:26:49 +0600

    header is added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="job-container">
+      <div className="header flex justify-between items-center">
+        <Link to="/">
+          <p className="text-2xl md:text-4xl font-extrabold text-orange-500 md:ml-6 sm:ml-3 ">JobSeeker</p>
+        </Link>
+        <div >
+          <ul className="hidden md:flex gap-5 items-center">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "nav-link" : "nav-link-inactive"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/statistics"
+                className={({ isActive }) =>
+                  isActive ? "nav-link" : "nav-link-inactive"
+                }
+              >
+                Statistics
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/applied"
+                className={({ isActive }) =>
+                  isActive ? "nav-link" : "nav-link-inactive"
+                }
+              >
+                Applied Jobs
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/blogs"
+                className={({ isActive }) =>
+                  isActive ? "nav-link" : "nav-link-inactive"
+                }
+              >
+                Blogs
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <button className="btn btn-primary bg-gradient-to-r from-blue-600 to-indigo-500 text-white">
+            Start Apply
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
